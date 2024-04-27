f94adc711fd65ea40bf26f1bfc9b1216abf4b1d4
Deepak Sharma <deepakfrontend04@gmail.com>
2024-04-27 22:44:30 +0530

    db connect

---
+++ src/index.js
@@ -0,0 +1,41 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({path:'./env'})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const app = express()
+
+(async ()=> {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("errer", error)
+        })
+        
+    app.listen(process.env.PORT, ()=> {
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+    } catch(error){
+        console.log("ERROR :", error)
+        throw err
+    }
+})()
+*/
